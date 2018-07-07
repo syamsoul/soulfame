01e2d1e2a8ec830e3bce92b90c788a40331a19c1
DESKTOP-5GQ71PR\mrSouL <syamsoulazrien.miat@gmail.com>
2018-07-07 20:37:06 +0800

    updated: Route variable

--- incl/class/Route.php
+++ incl/class/Route.php
@@ -38,10 +38,34 @@
             $current_page       = $this->filter_url_route($current_page);
             $pages              = $this->pages;
 
+            $current_page_arr = $this->get_url_level($current_page);
+
             foreach($pages as $route_url => $templ_file){
-                if($current_page == $route_url){
-                    $filename = $this->templ_folder . "/" . $templ_file;
-                    if(file_exists($filename)) return $filename;
+
+                $route_url_arr = $this->get_url_level($route_url);
+
+                if(count($current_page_arr) == count($route_url_arr)){
+                    $check_each = Array();
+
+                    foreach($current_page_arr as $j=>$e_cpa){
+                        $check_each[$j] = false;
+
+                        if(isset($route_url_arr[$j])){
+                            $is_url_var = $this->is_url_var($route_url_arr[$j]);
+                            if(is_array($is_url_var) && $is_url_var['result']){
+                                $_GET[$is_url_var['var_name']] = $e_cpa;
+                                $check_each[$j] = true;
+                            }else{
+                                if($route_url_arr[$j] == $e_cpa) $check_each[$j] = true;
+                            }
+                        }
+                    }
+
+                    if(!in_array(false, $check_each)) {
+                        $filename = $this->templ_folder . "/" . $templ_file;
+                        if(file_exists($filename)) return $filename;
+                        else break;
+                    }
                 }
             }
 
@@ -65,6 +89,27 @@
         	while(substr($page_str, -1, 1) == "/") $page_str = substr($page_str, 0, -1);
         	return $page_str;
 
+        }
+
+        private function get_url_level($page_str){
+            $page_str = $this->filter_url_route($page_str);
+            $page_str = explode("/", $page_str);
+            foreach($page_str as $i=>$e_ps) $page_str[$i] = trim($e_ps);
+            return $page_str;
+        }
+
+        private function is_url_var($sub_page_str){
+            $sub_page_str = trim($sub_page_str);
+
+            $result = (substr($sub_page_str, 0, 1) == "{") && (substr($sub_page_str, -1, 1) == "}");
+
+            $res = Array(
+                "result"    => $result,
+            );
+
+            if($result) $res['var_name'] = substr($sub_page_str, 1, -1);
+
+            return $res;
         }
 
     }
