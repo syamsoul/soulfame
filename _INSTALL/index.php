80b4561d522ab399bd60a49b3c3468788585e2a8
DESKTOP-5GQ71PR\mrSouL <syamsoulazrien.miat@gmail.com>
2018-07-10 05:08:34 +0800

    fixed: install form

--- _INSTALL/index.php
+++ _INSTALL/index.php
@@ -1,7 +1,22 @@
 <?php
     if(!file_exists(dirname(__FILE__) . "/core")) die("ERROR: No core file");
 
+    if(empty($_POST['dbregBtn'])){
+        $show_form = true;
+        $def_host_name  = "";
+        $def_db_name    = "";
+        $def_username   = "";
+    }else{
+        $conn = @mysqli_connect($_POST['host_name'], $_POST['username'], $_POST['password'], $_POST['db_name']);
+        $is_db_conn_failed = mysqli_connect_errno();
 
+        if($is_db_conn_failed){
+            $show_form = true;
+            $def_host_name  = $_POST['host_name'];
+            $def_db_name    = $_POST['db_name'];
+            $def_username   = $_POST['username'];
+        }else $show_form = false;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -10,33 +25,156 @@
         <title></title>
     </head>
     <body>
-        <?php if(empty($_POST['dbregBtn'])): ?>
-            <form class="" action="" method="post">
-                <div>
-                    <input type="text" name="host_name" value="" placeholder="HOST NAME">
-                </div>
-                <div>
-                    <input type="text" name="db_name" value="" placeholder="DB NAME">
-                </div>
-                <div>
-                    <input type="text" name="username" value="" placeholder="USERNAME">
-                </div>
-                <div>
-                    <input type="text" name="password" value="" placeholder="PASSWORD">
-                </div>
-                <div>
-                    <button type="submit" name="dbregBtn" value="true">Procceed</button>
-                </div>
-            </form>
+        <?php if($show_form): ?>
+            <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
+            <style type="text/css">
+            body{
+                font-family: 'Bitter', sans-serif !important;
+            }
+            .form-style-10{
+                width:450px;
+                padding:30px;
+                margin:40px auto;
+                background: #FFF;
+                border-radius: 10px;
+                -webkit-border-radius:10px;
+                -moz-border-radius: 10px;
+                box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+                -moz-box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+                -webkit-box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+            }
+            .form-style-10 .inner-wrap{
+                padding: 30px;
+                background: #F8F8F8;
+                border-radius: 6px;
+                margin-bottom: 15px;
+            }
+            .form-style-10 h1{
+                background: #2A88AD;
+                padding: 20px 30px 15px 30px;
+                margin: -30px -30px 30px -30px;
+                border-radius: 10px 10px 0 0;
+                -webkit-border-radius: 10px 10px 0 0;
+                -moz-border-radius: 10px 10px 0 0;
+                color: #fff;
+                text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.12);
+                font: normal 30px 'Bitter', serif;
+                -moz-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+                -webkit-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+                box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+                border: 1px solid #257C9E;
+            }
+            .form-style-10 h1 > span{
+                display: block;
+                margin-top: 2px;
+                font: 13px Arial, Helvetica, sans-serif;
+            }
+            .form-style-10 label{
+                display: block;
+                font: 13px Arial, Helvetica, sans-serif;
+                color: #888;
+                margin-bottom: 15px;
+            }
+            .form-style-10 input[type="text"],
+            .form-style-10 input[type="date"],
+            .form-style-10 input[type="datetime"],
+            .form-style-10 input[type="email"],
+            .form-style-10 input[type="number"],
+            .form-style-10 input[type="search"],
+            .form-style-10 input[type="time"],
+            .form-style-10 input[type="url"],
+            .form-style-10 input[type="password"],
+            .form-style-10 textarea,
+            .form-style-10 select {
+                display: block;
+                box-sizing: border-box;
+                -webkit-box-sizing: border-box;
+                -moz-box-sizing: border-box;
+                width: 100%;
+                padding: 8px;
+                border-radius: 6px;
+                -webkit-border-radius:6px;
+                -moz-border-radius:6px;
+                border: 2px solid #fff;
+                box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.33);
+                -moz-box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.33);
+                -webkit-box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.33);
+            }
+
+            .form-style-10 .section{
+                font: normal 20px 'Bitter', serif;
+                color: #2A88AD;
+                margin-bottom: 5px;
+            }
+            .form-style-10 .section span {
+                background: #2A88AD;
+                padding: 5px 10px 5px 10px;
+                position: absolute;
+                border-radius: 50%;
+                -webkit-border-radius: 50%;
+                -moz-border-radius: 50%;
+                border: 4px solid #fff;
+                font-size: 14px;
+                margin-left: -45px;
+                color: #fff;
+                margin-top: -3px;
+            }
+            .form-style-10 input[type="button"],
+            .form-style-10 button{
+                background: #2A88AD;
+                padding: 8px 20px 8px 20px;
+                border-radius: 5px;
+                -webkit-border-radius: 5px;
+                -moz-border-radius: 5px;
+                color: #fff;
+                text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.12);
+                font: normal 30px 'Bitter', serif;
+                -moz-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+                -webkit-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+                box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+                border: 1px solid #257C9E;
+                font-size: 15px;
+            }
+            .form-style-10 input[type="button"]:hover,
+            .form-style-10 button:hover{
+                background: #2A6881;
+                -moz-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.28);
+                -webkit-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.28);
+                box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.28);
+            }
+            .form-style-10 .privacy-policy{
+                float: right;
+                width: 250px;
+                font: 12px Arial, Helvetica, sans-serif;
+                color: #4D4D4D;
+                margin-top: 10px;
+                text-align: right;
+            }
+            </style>
+            <div class="form-style-10">
+                <h1 style="text-align:center;">SoulFame<span>Please enter the application details to procceed</span></h1>
+                <form action="" method="POST">
+                    <div class="section"><span>1</span>MySQL Database</div>
+                    <div class="inner-wrap">
+                        <label>Host Name <input type="text" name="host_name" value="<?=$def_host_name?>"/></label>
+                        <label>Database Name <input type="text" name="db_name" value="<?=$def_db_name?>"/></label>
+                        <label>Username <input type="text" name="username" value="<?=$def_username?>"/></label>
+                        <label>Password <input type="password" name="password" /></label>
+                        <?= ((isset($is_db_conn_failed) && $is_db_conn_failed) ? "<div style=\"color:red;\">ERROR: Failed to connect to MySQL: " . mysqli_connect_error() . "</div>" : "") ?>
+                    </div>
+
+                    <div class="button-section">
+                        <button type="submit" name="dbregBtn" value="true">Procceed</button>
+                        <span class="privacy-policy" style="display:none;">
+                            <input type="checkbox" name="field7">You agree to our Terms and Policy.
+                        </span>
+                    </div>
+                </form>
+            </div>
         <?php endif; ?>
 
         <?php if(!empty($_POST['dbregBtn'])){
-            $conn = @mysqli_connect($_POST['host_name'], $_POST['username'], $_POST['password'], $_POST['db_name']);
-
-            // Check connection
-            if (mysqli_connect_errno()){
-                echo "Failed to connect to MySQL: " . mysqli_connect_error();
-            }else{
+            if(!$is_db_conn_failed){
                 $temp_pass = generateRandomString(50);
 
                 $params = "data=".sd_encrypt(json_encode(Array(
@@ -163,7 +301,7 @@
                 <script type="text/javascript">
                     setTimeout(function(){
                         window.location.href = "install.php?<?=$params?>";
-                    },500);
+                    },1500);
                 </script>
             <?php
             }
